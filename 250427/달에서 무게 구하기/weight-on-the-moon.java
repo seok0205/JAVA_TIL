65207c2f5464c553aef7f1b34f840f4f7e8d57f9
Jungseok Yu <101177749+seok0205@users.noreply.github.com>
2025-04-27 22:00:35 +0900

    Title: 달에서 무게 구하기, Time: 93ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250427/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int weight = 13;
+        double gravity = 0.165;
+        System.out.printf(weight + " * %.6f = %.6f", gravity, weight*gravity);
+    }
+}
