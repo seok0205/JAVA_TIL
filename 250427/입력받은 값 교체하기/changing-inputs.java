21f1c89236c1217154a5c4e99a96bb9041f5f04e
Jungseok Yu <101177749+seok0205@users.noreply.github.com>
2025-04-27 22:32:42 +0900

    Title: 입력받은 값 교체하기, Time: 114ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250427/입력받은 값 교체하기/changing-inputs.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner input = new Scanner(System.in);
+
+        String a = input.next();
+        String b = input.next();
+        System.out.println(b + ' ' + a);
+    }
+}
