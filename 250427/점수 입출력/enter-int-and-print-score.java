d03bda5180690a7d4a8ec8ca0cf2fbc5823b146f
Jungseok Yu <101177749+seok0205@users.noreply.github.com>
2025-04-27 22:13:40 +0900

    Title: 점수 입출력, Time: 131ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250427/점수 입출력/enter-int-and-print-score.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner input = new Scanner(System.in);
+        int N = input.nextInt();
+        System.out.println("Your score is " + N + " point.");
+    }
+}
