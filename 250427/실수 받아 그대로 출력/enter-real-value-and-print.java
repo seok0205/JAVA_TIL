3fb65901107c75cc20303c3729b047babc7064ab
Jungseok Yu <101177749+seok0205@users.noreply.github.com>
2025-04-27 22:19:17 +0900

    Title: 실수 받아 그대로 출력, Time: 153ms, Memory: 12MB, Status: Passed - Codetree

---
+++ 250427/실수 받아 그대로 출력/enter-real-value-and-print.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner input = new Scanner(System.in);
+        double N = input.nextDouble();
+        System.out.printf("%.2f", N);
+    }
+}
