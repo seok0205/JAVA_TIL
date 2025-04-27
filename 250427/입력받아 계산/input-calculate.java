8c0570a16c340682e89efe703ccd0f29451dc23d
Jungseok Yu <101177749+seok0205@users.noreply.github.com>
2025-04-27 22:16:35 +0900

    Title: 입력받아 계산, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250427/입력받아 계산/input-calculate.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        a += 2;
+        System.out.println(a);
+    }
+}
